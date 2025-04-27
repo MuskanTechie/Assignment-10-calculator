3182f742602e4ca8b7a5d370dbe182f16b79f3b9
MuskanTechie <syedamuskanfatima512@gmail.com>
2025-04-27 23:51:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+//alert("This is working!!!")
+//addition
+
+let num1= parseFloat(prompt("Enter 1st number:"));
+let num2= parseFloat(prompt("Enter 2nd number:"));
+let sum=num1+num2;
+alert("The sum is:" + sum);
+
+//subtraction
+/*
+let num1= parseFloat(prompt("Enter 1st number:"));
+let num2= parseFloat(prompt("Enter 2nd number:"));
+let difference = num1 - num2;
+alert("The difference is:" + difference); 
+*/
+//multiplication
+/*
+let num1= parseFloat(prompt("Enter 1st number:"));
+let num2= parseFloat(prompt("Enter 2nd number:"));
+let Multiply = num1 * num2;
+alert(" The answer of multiplication is:" + Multiply); 
+*/
+//division
+/*
+let num1= parseFloat(prompt("Enter 1st number:"));
+let num2= parseFloat(prompt("Enter 2nd number:"));
+let Divide = num1 / num2;
+alert(" The answer of division is:" + Divide); 
+*/
